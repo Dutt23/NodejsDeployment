bfe238093cee4d9a5e13986759af800f11671a05
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-07-18 22:46:20 +0530

    Second

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI:
-      "mongodb://Shatyaki:Shatyak2@ds141611.mlab.com:41611/vidjot-prod"
+    mongoURI: "mongodb://Shatyaki:Shatyak2@ds141611.mlab.com:41611/vidjot-prod"
   };
 } else {
-    module.exports={
-        mongoURI:"mongodb://localhost:27017/vidjot-dev"
-    }
+  module.exports = {
+    mongoURI: "mongodb://localhost:27017/vidjot-dev"
+  };
 }
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const session = require("express-session");
 // Path module , core module. Used to redirect paths
 const path = require("path");
 // DB config
-const db = require("./config/database")
+const db = require("./config/database");
 // Load routes
 const ideas = require("./routes/ideas");
 const users = require("./routes/users");
@@ -36,7 +36,6 @@ app.engine(
 );
 
 app.set("view engine", "handlebars");
-
 
 // Middleware for body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -69,6 +68,7 @@ mongoose
     }
   )
   .then(() => {
+    
     console.log("Mongo Connected");
   })
   .catch(() => {
@@ -79,7 +79,7 @@ mongoose
 // Global variables
 app.use((req, res, next) => {
   //   console.log(Date.now());
-  
+
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
@@ -91,7 +91,6 @@ app.use((req, res, next) => {
 // Use routes
 app.use("/ideas", ideas);
 app.use("/users", users);
-
 
 // Viewing all ideas
 // Fetches everything from the db
